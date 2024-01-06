2ee4b98be7f86b2b1058ba0a00bf2e8ce485e0e5
Djennad Rany <128905298+DjennadRany@users.noreply.github.com>
2024-01-06 19:38:52 +0100

    correctif regex jpg
    correctif regex jpg 
    
    mise en place du regex jpg et correction des test newBill.js

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,15 +18,25 @@ export default class NewBill {
     new Logout({ document, localStorage, onNavigate })
   }
   handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
-
+    e.preventDefault();
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    const file = fileInput.files[0];
+  
+    // Vérifier si le fichier n'est pas au format JPEG
+    if (!/\.(jpe?g)$/i.test(file.name)) {
+      alert("Veuillez sélectionner un fichier au format JPEG.");
+      // Réinitialiser la valeur du champ de fichier
+      fileInput.value = '';
+      return;
+    }
+  
+    const filePath = fileInput.value.split(/\\/g);
+    const fileName = filePath[filePath.length - 1];
+    const formData = new FormData();
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    formData.append('file', file);
+    formData.append('email', email);
+  
     this.store
       .bills()
       .create({
@@ -35,13 +45,14 @@ export default class NewBill {
           noContentType: true
         }
       })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
-  }
+      .then(({ fileUrl, key }) => {
+        console.log(fileUrl);
+        this.billId = key;
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+      })
+      .catch(error => console.error(error));
+  };
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
